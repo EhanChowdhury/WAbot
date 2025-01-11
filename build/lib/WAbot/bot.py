9028cdfd95acc67c63dade61237484d7555c7c64
Ehan Chowdhury <nibizsoft@gmail.com>
2025-01-12 02:55:36 +0600

    Updated changes

--- build/lib/WAbot/bot.py
+++ build/lib/WAbot/bot.py
@@ -12,7 +12,7 @@ from typing import List, Dict
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import copykitten
+import pyclip
 
 
 
@@ -74,7 +74,7 @@ class WAbot:
             message_Element.click()
             self.log("Clicked message box successfully...")
             time.sleep(humanize)
-            copykitten.copy(msg)
+            pyclip.copy(msg)
             self.log("using clipboard")
             paste_key = Keys.CONTROL if platform.system() in ["Windows", "Linux"] else Keys.COMMAND
             message_Element.send_keys(paste_key, 'v')
