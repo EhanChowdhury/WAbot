b66fe0c7a142cfb234d9932645194099464912ae
Ehan Chowdhury <nibizsoft@gmail.com>
2025-01-12 23:34:38 +0600

    Updated changes

--- WAbot/bot.py
+++ WAbot/bot.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import pyperclip
 from typing import List, Dict
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
